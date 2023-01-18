279a39c8dd67e642b0e405c3c7f49aa7b810e7df
caiooliveirah09 <leelros@hotmail.com>
2023-01-18 16:47:20 -0300

    feat: add cars route

--- src/Routes/cars.route.ts
+++ src/Routes/cars.route.ts
@@ -3,6 +3,7 @@ import CarsController from '../Controllers/CarsController';
 
 const router = Router();
 
-router.post('/', (req, res, next) => new CarsController(req, res, next).registerACar());
-
+router.post('/', (req, res, next) => new CarsController(req, res, next).create());
+router.get('/', (req, res, next) => new CarsController(req, res, next).find());
+router.get('/:id', (req, res, next) => new CarsController(req, res, next).findById());
 export default router;
