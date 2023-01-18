c6fbc38434c26aec9444f0ebb3b5a9f0a14ad0bf
caiooliveirah09 <leelros@hotmail.com>
2023-01-18 16:48:04 -0300

    feat: add CarsController.ts and CarsService.ts

--- src/Controllers/CarsController.ts
+++ src/Controllers/CarsController.ts
@@ -1,17 +1,34 @@
 import { NextFunction, Request, Response } from 'express';
+import ICar from '../Interfaces/ICar';
+import CarsService from '../Services/CarsService';
+import StatusHTTP from '../utils/StatusHTTP';
 
 export default class CarsController {
   private req: Request;
   private res: Response;
   private next: NextFunction;
+  private service: CarsService;
 
   constructor(req: Request, res: Response, next: NextFunction) {
     this.req = req;
     this.res = res;
     this.next = next;
+    this.service = new CarsService();
+  }
+
+  public async create() {
+    const { status, message } = await this.service.create(this.req.body as ICar);
+    return this.res.status(status).json(message);
   }
   
-  registerACar() {
-    return this.res.status(200).json('salve');
+  public async find() {
+    const { status, message } = await this.service.find();
+    return this.res.status(status).json(message);
+  }
+
+  public async findById() {
+    const { status, message } = await this.service.findById(this.req.params.id as string);
+    if (status === StatusHTTP.OK) return this.res.status(status).json(message);
+    return this.res.status(status).json({ message });
   }
 }
---
+++ src/Services/CarsService.ts
@@ -0,0 +1,35 @@
+import Car from '../Domains/Car';
+import ICar from '../Interfaces/ICar';
+import IController from '../Interfaces/IController';
+import CarODM from '../Models/CarODM';
+import StatusHTTP from '../utils/StatusHTTP';
+
+export default class CarsService {
+  private createCarDomain(car: ICar | null) : Car | null {
+    if (car) return new Car(car);
+    return null;
+  }
+
+  public async create(car: ICar): Promise<IController> {
+    const carODM = new CarODM();
+    const newCar = await carODM.create(car);
+    return { status: StatusHTTP.CREATED, message: this.createCarDomain(newCar) };
+  }
+
+  public async find(): Promise<IController> {
+    const carODM = new CarODM();
+    const allCars = await carODM.find();
+    return { status: StatusHTTP.OK, message: allCars.map((car) => this.createCarDomain(car)) };
+  }
+
+  public async findById(id: string): Promise<IController> {
+    try {
+      const carODM = new CarODM();
+      const oneCar = await carODM.findById(id);
+      if (!oneCar) return { status: StatusHTTP.NOT_FOUND, message: 'Car not found' };
+      return { status: StatusHTTP.OK, message: this.createCarDomain(oneCar) };
+    } catch (error) {
+      return { status: StatusHTTP.UNPROCESSABLE_ENTITY, message: 'Invalid mongo id' };
+    }
+  }
+}
