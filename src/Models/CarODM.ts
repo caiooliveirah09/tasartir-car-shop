adeaefb888800a8c182794525b2ace6679eefa6c
caiooliveirah09 <leelros@hotmail.com>
2023-01-18 16:47:02 -0300

    feat: add CarODM.ts

---
+++ src/Models/CarODM.ts
@@ -0,0 +1,35 @@
+import { Model, Schema, model, models } from 'mongoose';
+import ICar from '../Interfaces/ICar';
+
+export default class CarODM {
+  private schema: Schema;
+  private model: Model<ICar>;
+
+  constructor() {
+    this.schema = new Schema<ICar>({
+      model: { type: String, required: true },
+      year: { type: Number, required: true },
+      color: { type: String, required: true },
+      status: { type: Boolean, required: false },
+      buyValue: { type: Number, required: true },
+      doorsQty: { type: Number, required: true },
+      seatsQty: { type: Number, required: true },
+    });
+    this.model = models.cars || model<ICar>('cars', this.schema);
+  }
+
+  public async create(car: ICar): Promise<ICar> {
+    const newCar = await this.model.create({ ...car });
+    return newCar;
+  }
+
+  public async find(): Promise<ICar[]> {
+    const cars = await this.model.find();
+    return cars;
+  }
+
+  public async findById(id: string): Promise<ICar | null > {
+    const car = await this.model.findById(id);
+    return car;
+  }
+}
